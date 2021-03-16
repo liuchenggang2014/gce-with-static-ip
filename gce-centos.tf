62b99f5b55796236f637d243a66c4cf6e02be90c
chenggang liu@g <liuchenggang@google.com>
2021-03-16 12:41:11 +0000

    main.tf

--- gce-centos.tf
+++ gce-centos.tf
@@ -1,12 +1,3 @@
-provider "google" {
-#   version = "3.5.0"
-  # Update credentials to the correct location, alternatively set   GOOGLE_APPLICATION_CREDENTIALS=/path/to/.ssh/bq-key.json in your shell session and   remove the credentials attribute.
-  credentials = file("~/cliu201-sa.json")
-  project     = "cliu201"
-  region      = "us-central1"
-  zone        = "us-central1-c"
-}
-
 
 resource "google_compute_instance" "default" {
   name         = "cliu201-centos8"
